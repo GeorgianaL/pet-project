31f99aa990300faa514e71bc1dcb24590b5dad93
GeorgianaL <georgiana@patternex.com>
2018-07-20 14:50:40 +0300

    Create first components

---
+++ src/containers/app/app.component.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../../style.scss';
+
+import Header from '../header';
+import Main from '../main';
+
+const App = () => (
+  <div>
+    <Header />
+    <Main />
+  </div>
+);
+
+export default App;
---
+++ src/containers/main/main.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Main = () => (
+  <div>
+    Main component
+  </div>
+);
+
+export default Main;
